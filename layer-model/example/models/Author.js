91ca0dfb8e0a490842e6e79479b25dd9e9c2e12d
WalterTNFC <walter.tad@hotmail.com>
2022-04-11 16:21:03 -0300

    Criacao da funcao para alterar a chave para ser Camel Case e cricao da funcao para adiconar o nome completo do Author

--- layer-model/example/models/Author.js
+++ layer-model/example/models/Author.js
@@ -1,6 +1,25 @@
 // para usar comandos sql
 const connection = require('./connection');
 
+function getNewAuthor({id, firstName, middleName, lastName}) {
+  const fullName = [firstName, middleName, lastName].filter((value) => value).join(" ");
+  return {
+    id,
+    firstName,
+    middleName,
+    lastName,
+    fullName
+  }
+}
+
+function serialize(authorData) {
+  return {
+    id: authorData.id,
+    firstName: authorData.first_name,
+    middleName: authorData.middle_mame,
+    lastName: authorData.last_name,
+  };
+}
 // fluxo assincrono por trabalhar com banco
 // de dados é custoso
 // executa uma query do banco de dados e retorna esse valor
@@ -8,7 +27,7 @@ async function getAll() {
   // recebe o valor do obj connection
   const [authors] = await connection.execute('SELECT id, first_name, middle_name, last_name FROM authors');
 
-  return authors;
+  return authors.map(serialize).map(getNewAuthor);
 };
 
 module.exports = {
