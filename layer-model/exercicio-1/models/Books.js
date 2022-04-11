6c73f28b0f21695f1b98c9c1318369e354cb6a4c
WalterTNFC <walter.tad@hotmail.com>
2022-04-11 17:17:12 -0300

    Criacao do metodo getAll

---
+++ layer-model/exercicio-1/models/Books.js
@@ -0,0 +1,10 @@
+const connection = require('./connection');
+
+async function getAll(req, res) {
+  const result = await connection.execute('SELECT title, author_id FROM books') 
+  return result;
+}
+
+module.exports = {
+  getAll,
+}
