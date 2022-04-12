0c5a8b55db385dc7140d5a1e76643f9f6149e0c7
WalterTNFC <walter.tad@hotmail.com>
2022-04-11 22:49:07 -0300

    Criacao da funcao getBookById para buscar por id especifico

--- layer-model/exercicio-1/models/Books.js
+++ layer-model/exercicio-1/models/Books.js
@@ -1,10 +1,17 @@
 const connection = require('./connection');
 
 async function getAll() {
-  const result = await connection.execute('SELECT title, author_id FROM books');
-  return result;
+  const [books] = await connection.execute('SELECT title, author_id FROM books');
+  return books;
 };
+
+async function getBookById(authorId) {
+  const query = 'SELECT id, title, author_id FROM books WHERE author_id=?;'
+  const [booksById] = await connection.execute(query,[authorId])
+  return booksById;
+}
 
 module.exports = {
   getAll,
+  getBookById,
 }
