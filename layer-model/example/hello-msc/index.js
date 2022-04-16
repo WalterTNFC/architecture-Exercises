ec9d011fdb09496fa0dd625cefbde490da212535
WalterTNFC <walter.tad@hotmail.com>
2022-04-16 17:10:50 -0300

    Criacao da endPoint get para retorno de todos os autores

--- layer-model/example/hello-msc/index.js
+++ layer-model/example/hello-msc/index.js
@@ -1,4 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const Authors = require('./models/Author');
+const Author = require('./models/Author');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/authors', async (_req, res) => {
+  // pegar a função dentro do model
+  const authors = await Author.getAll();
+  // definição do status 200
+  res.status(200).json(authors)
+})
