27eb0dfc9a3f2a4dce4ea533c836f782fa057972
WalterTNFC <walter.tad@hotmail.com>
2022-04-16 17:29:10 -0300

    Funcao para encontrar a partir do ID

--- layer-model/example/hello-msc/index.js
+++ layer-model/example/hello-msc/index.js
@@ -8,8 +8,21 @@ const app = express();
 app.use(bodyParser.json());
 
 app.get('/authors', async (_req, res) => {
-  // pegar a função dentro do model
+  // acessar a função dentro do model
   const authors = await Author.getAll();
   // definição do status 200
   res.status(200).json(authors)
-})
+});
+
+app.get('/authors', async (req, res) => {
+  // acessar a função para encontrar um author a partir do id
+  const { id } = req.params;
+
+  const author = await Author.findById();
+
+  if(!author) {
+    return res.status(404).json({message: 'Not found'});
+  }
+
+  res.status(200).json(author);
+});
